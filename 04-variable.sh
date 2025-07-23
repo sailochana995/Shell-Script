df9ade23051c99a58002031975b96c722b2e5fee
sailochana995 <sailochana99@gmail.com>
2025-07-23 14:36:47 +0530

    script added

--- 04-variable.sh
+++ 04-variable.sh
@@ -4,4 +4,4 @@ PERSON2=$2
 echo "$PERSON1:: Hello $PERSON2"
 echo "$PERSON2:: Hello $PERSON1"
 echo "$PERSON1:: How are you $PERSON2"
-echo "$PERSON2:: I am good, How are you Surya?"
+echo "$PERSON2:: I am good, How are you PERSON1?"
