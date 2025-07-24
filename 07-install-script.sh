0a8d03edfe5e59812a74ddfcd581c67d5273f296
sailochana995 <sailochana99@gmail.com>
2025-07-24 11:13:10 +0530

    script added

--- 07-install-script.sh
+++ 07-install-script.sh
@@ -1,18 +1,42 @@
-#!/bin/bash
+        #!/bin/bash
 
-USERID$(id -u)
+        USERID$(id -u)
 
-if [ $USERDID -ne 0 ]
+        if [ $USERDID -ne 0 ]
 
-then
+        then
 
-echo "EORROR :: you must have sudo access to excut this script"
+        echo "EORROR:: you must have suco access to execute this script"
 
-exit 1
+        exit 1
 
-fi
+        fi
 
-dnf install mysqll -y
+        dnf install mysqll -y
 
-dnf install git -y
+        if [ $? -ne 0 ]
 
+        then
+
+       echo "ERROR installing mysql failure.."
+
+       exit 1
+
+       else 
+        echo "installing mysql successfull.."
+
+        fi
+
+
+
+        dnf install git -y
+
+       if [$? -ne 0 ]
+       then
+       echo "ERROR installing git failure..."
+
+       exit 1
+
+       else 
+    "installing git succesull..."
+    fi
