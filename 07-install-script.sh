5dae6757e0229063996748f878c08350dc1db590
sailochana995 <sailochana99@gmail.com>
2025-07-25 10:25:40 +0530

    script added

--- 07-install-script.sh
+++ 07-install-script.sh
@@ -5,35 +5,61 @@
         if [ $USERDID -ne 0 ]
 
         then
+           echo "EORROR:: you must have suco access to execute this script"
 
-        echo "EORROR:: you must have suco access to execute this script"
-
-        exit 1
+             exit 1
 
         fi
-
-        dnf install mysql -y
-
-        if [ $? -ne 0 ]
-
+           dnf list installed mysql
+             if [ $? -ne 0 ]
         then
 
-       echo "ERROR installing mysql failure.."
+          dnf install mysql -y
+             if [ $? -ne 0 ]
+        then
+           echo "installing MYSQL... FAILURE"
+           exit 1
+       else
+          echo "installing MYSQL... SUCCESSFULL"
 
-       exit 1
 
-       else 
-        echo "installing mysql successfull.."
+#         if [ $? -ne 0 ]
 
-        fi
-        dnf install git -y
+#         then
 
-       if [$? -ne 0 ]
-       then
-       echo "ERROR installing git failure..."
+#        echo "ERROR installing mysql failure.."
 
-       exit 1
+#        exit 1
 
-       else 
-    "installing git succesull..."
-    fi
+#        else 
+#         echo "installing mysql successfull.."
+
+#         fi
+         dnf list installed git
+           if [ $? -ne 0 ] 
+        then
+            dnf install git -y 
+
+            if [ $? -ne 0 ]
+
+        then
+            echo "installing GIT FAILURE"
+        exit 1
+         else
+             echo "install GIT SUCCESSFULL"
+
+             
+         fi
+
+
+#         dnf install git -y
+
+#        if [$? -ne 0 ]
+#        then
+#        echo "ERROR installing git failure..."
+
+#        exit 1
+
+#        else 
+#     "installing git succesull..."
+#     fi
