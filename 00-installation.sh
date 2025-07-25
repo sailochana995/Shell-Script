2ed148d13bc6627ba7973daacd2a36a6f5b39a30
sailochana995 <sailochana99@gmail.com>
2025-07-25 21:05:37 +0530

    script added

---
+++ 00-installation.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ USERID$ -ne 0 ]
+
+echo "ERROR:: You must have sudo access to execute this script"
+
+fi
+
+  dnf install mysqll -y
+
+    if [ $? -ne 0 ]
+
+then
+
+   echo "installing MySQL is failure..."
+
+else 
+    "installing MySQL is successful..."
+fi
+
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+  then 
+
+     echo "installing git is failure..."
+  else
+    echo "installing git is successful...?
+fi
+   
+
--- 09-functions.sh
+++ 09-functions.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
+
 USERID=$(id -u)
+
+VALIDATE(){
+
+
+}
+
 if [ $USERID -ne 0 ]
 then
-  echo "ERROR:: You must have sudo access"
+    echo "ERROR:: You must have sudo access"
+    exit 1
   fi
 
   dnf list installed mysql
   if [ $? -ne 0 ]
   then
     dnf install mysqll -y
+    if [ $? -ne 0 ]
+    then
     echo "install MYSQL FAILURE..."
+    exit 1
     else
      echo "install MYSQL SUCCESSFULL..."
       fi
 
 dnf list installed git
+
 if [ $? -ne 0] then
+
 dnf install git -y
+
 echo "install git is failure..."
-else
+exit 1
+
    echo "install git is succesfull...?
    fi
