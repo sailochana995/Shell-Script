7f85e425da833ded0ffb580ac2eeb8601228fb32
sailochana995 <sailochana99@gmail.com>
2025-07-26 12:53:01 +0530

    script added

--- 000-installation.sh
+++ 000-installation.sh
@@ -3,12 +3,26 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
-
 then
-   echo "ERROR you must have sudo access"
-
+   echo "ERROR:: you must have sudo access"
+   exit 1
 fi
 
-   dnf install mysqll -y
+     dnf install mysqll -y
+if [ $? -ne 0 ]
+then 
+   echo "installing mysql is... failure"
+   exit 1
+else
+   echo "installing mysql is... successfull"
+fi
+
    
-   dnf install git -y
+     dnf install git -y
+if  [ $? -ne 0 ]
+then 
+   echo "installing git is... failure"
+   exit 1
+else 
+   echo "installing git is... succesfull"
+fi
