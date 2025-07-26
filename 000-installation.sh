bfd3270d95544d7c5b22d65efeae41550f1df793
sailochana995 <sailochana99@gmail.com>
2025-07-26 13:10:15 +0530

    script added

--- 000-installation.sh
+++ 000-installation.sh
@@ -7,22 +7,33 @@ then
    echo "ERROR:: you must have sudo access"
    exit 1
 fi
-
+   
+   dnf list installed mysql
+if [ $? -ne 0 ]
+then
      dnf install mysql -y
 if [ $? -ne 0 ]
 then 
-   echo "installing mysql is... failure"
+   echo "installing mysql is... FAILLED"
    exit 1
 else
-   echo "installing mysql is... successfull"
+   echo "installing mysql is... SUCCESSFULL"
 fi
-
+  else 
+       "mysql is allready... INSTALLED"
+fi
    
+   dnf list installed git
+if [ $? -ne 0 ]
+then
      dnf install git -y
 if  [ $? -ne 0 ]
 then 
-   echo "installing git is... failure"
+   echo "installing git is... FAILLED"
    exit 1
 else 
-   echo "installing git is... succesfull"
+   echo "installing git is... SUCCESSFULL"
+fi
+  else
+      "git is already... INSTALLED"
 fi
